ea22e937092ec8ae19f8789493afd5fb3cf5db9c
istobarton <istobarton@gmail.com>
2015-06-12 16:04:08 -0500

    trees & graphs pass

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,46 @@
 
 
 var Graph = function(){
+  //properties that are needed at time of creation
+  //and different for each Graph node
+  this.gObj = {};
+  this.connection = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.gObj[node] = node;
 };
 
 Graph.prototype.contains = function(node){
+  return _.contains(this.gObj,node);
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.gObj[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var edge1 = fromNode + toNode;
+  var edge2 = toNode + fromNode;
+  return _.contains(this.connection, edge1) || _.contains(this.connection, edge2);
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  var edge1 = fromNode + toNode;
+  var edge2 = toNode + fromNode;
+  this.connection[edge1] = edge1;
+  this.connection[edge2] = edge2
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var edge1 = fromNode + toNode;
+  var edge2 = toNode + fromNode;
+  delete this.connection[edge1];
+  delete this.connection[edge2];
 };
 
 Graph.prototype.forEachNode = function(cb){
+  _.each(this.gObj, cb);
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree,treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,11 +16,25 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target){
-
+  var length = this.children.length || 0;
+  var isFound = false;
+  if(this.value===target){
+    isFound = true;
+    return isFound;
+  }
+  else if(length>0){
+    for(var i = 0; i<length; i++){
+      if(this.children[i].contains(target)){
+        isFound = true
+      }
+    }
+  }
+  return isFound;
 };
 
 
